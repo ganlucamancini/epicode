aa4da8a8294dfed11b21f01d4932bc4b3a039cca
nomeutente <miaemail@prova.com>
2024-02-09 17:30:36 +0100

    no mod

--- 1 m/3s/5g/D10.js
+++ 1 m/3s/5g/D10.js
@@ -88,9 +88,8 @@ console.log("Il numero più grande è:", whoIsBigger(10, 5)); // Output: 10
 
   Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
 */
-
+let sentence = "ciao sono gianluca";
 function splitMe(sentence) {
-  let sentence = "ciao sono gianluca";
   return sentence.split(" ");
 }
 console.log("Array di parole:", splitMe(sentence));
